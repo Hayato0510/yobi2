5dfa097cfc23f588dcd66710958295a9efaacda1
EC2 Default User <ec2-user@ip-172-31-21-175.us-east-2.compute.internal>
2021-03-02 21:16:38 +0000

    m

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,20 @@
 // LP獲得用(1)
 Route::get('info1', function () {
     return view('fb.check'); //提出用
-    // return view('fb.pp'); //ワクワク - P活LP
+    // return view('fb.job'); //チャットレディー案件訴求
+    
+    // return view('fb.pp'); //ワクワク - P活LP - LINE@を活用する場合は3日おきにチャトレ案件訴求 & 作り変え
 });
 
 // LP確認用
 Route::get('cccheck', function () {
-    return view('fb.pp'); //ワクワク - P活LP
+    return view('fb.job'); //チャットレディー案件訴求
+    
+    // return view('fb.pp'); //ワクワク - P活LP
 });
+
+//OS識別 - チャットレディー
+Route::get('info', 'AdController@info');
 
 //OS識別 - パパ活
 Route::get('os', 'AdController@os');
